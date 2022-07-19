a08f1b6f600d28a0723a3fdbff25f654f1db8ce9
Camilla Poushyk <camilla.poushyk@gmail.com>
2022-07-19 17:54:32 -0400

    commit tree 9001dffa37ec8eb715a3f66e0d68f5e92a604d49

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arry) {
+    return arry.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(arry) {
+    return arry.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMulti) {
+    return function(fare) { return fare * fareMulti}
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arry, divers) {
+    return divers(arry);
+}
